b649112d716716a96ef0096f50377a73d112a28a
Keith Tharp <keithstharp@gmail.com>
2020-11-09 11:34:41 -0500

    about formatting finished

--- src/views/About.tsx
+++ src/views/About.tsx
@@ -3,12 +3,10 @@ import React from "react";
 export default function About() {
   return (
     <section className="view-section">
+      <img className="about-photo" alt="Keith Tharp" src="./images/keith-tharp.jpg" />
       <div className="view-container">
-        <img className="about-photo" alt="Keith Tharp" src="./images/keith-tharp.jpg" />
-        <h2>About Me</h2>
-        <p><b>Full-stack web developer</b> building on my marketing and entrepreneurial background to
-        create enjoyable web experiences.</p>
-        <p>As a developer I enjoy combining the creativity and problem solving that I bring from my past careers into
+        <p>I'm a <b>Full-stack web developer</b> building on my marketing and entrepreneurial background to
+        create enjoyable web experiences. As a developer I enjoy combining the creativity and problem solving that I bring from my past careers into
         real solutions for clients and internet users.
         I've worked in several great groups within all aspects of app development and recently completed a media app
         that helps users log and save their media consumption.
