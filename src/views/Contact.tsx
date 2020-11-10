76d0b448710a1e319eda1160946b56033f39c9bd
Keith Tharp <keithstharp@gmail.com>
2020-11-10 08:48:12 -0500

    Adjust contact for 🚗  &  🔨 build 🏠

--- src/views/Contact.tsx
+++ src/views/Contact.tsx
@@ -9,7 +9,7 @@ export default function Contact() {
       <div className="view-container">
         <img className="contact-photo" alt="Keith Tharp" src="./images/keith-tharp.jpg" />
         <ul className="contact-list">
-          <li><span><img className="contact-icon" src="./images/phone.png" alt="phone icon" />6O3.49O.O8IO - Call || Text</span></li>
+          <li><span><img className="contact-icon" src="./images/phone.png" alt="phone icon" />6O3.49O.O8IO Call || Text</span></li>
           <li><span><img className="contact-icon" src="./images/mail.png" alt="email icon" />KeithSTharp@gmail.com</span></li>
           <li><a href="https://github.com/keiththarp"><img className="contact-icon" src="./images/github-icon.png" alt="github icon" />View my repos on Github</a></li>
           <li><a href="https://www.linkedin.com/in/keithstharp/"><img className="contact-icon" src="./images/linkedIn.png" alt="linkedIn icon" />My LinkedIn profile</a></li>
