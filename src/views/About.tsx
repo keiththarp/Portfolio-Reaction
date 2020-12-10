6363a242fa31b255fdd910eb3063d9894f407947
Keith Tharp <keithstharp@gmail.com>
2020-12-10 15:52:06 -0500

    Updates and build

--- src/views/About.tsx
+++ src/views/About.tsx
@@ -6,7 +6,7 @@ export default function About() {
       <img className="about-photo" alt="Keith Tharp" src="./images/keith-tharp.jpg" />
       <div className="view-container">
         <p><b>Full-stack engineer</b> building on my marketing and entrepreneurial background to create enjoyable web experiences. Recently earned a certificate in full stack development from the University of New Hampshire, increasing my HTML and CSS knowledge while adding JavaScript, React.js, and database systems. As a developer I enjoy combining the creativity and problem solving that I bring from my past careers into real solutions for clients and internet users. I've worked in several great groups within all aspects of app development one of which recently completed development of Ascent Sobriety, an app for folks in recovery, a purpose close to my heart. The app's driving concept is non-linear sobriety tracking with the intent of encouraging those in recovery to stay on their journey, through and beyond struggles and hiccups.</p>
-        <p><a href="https://bit.ly/KT-Resume">My resume.</a></p>
+        <p><a className="resume" href="https://bit.ly/KT-Resume">My resume.</a></p>
       </div>
       <div className="view-buffer"></div>
     </section>
--- src/views/Contact.tsx
+++ src/views/Contact.tsx
@@ -11,11 +11,13 @@ export default function Contact() {
         <ul className="contact-list">
           <li><span><img className="contact-icon" src="./images/phone.png" alt="phone icon" />6O3.49O.O8IO Call || Text</span></li>
           <li><span><img className="contact-icon" src="./images/mail.png" alt="email icon" />KeithSTharp@gmail.com</span></li>
-          <li><a href="https://github.com/keiththarp"><img className="contact-icon" src="./images/github-icon.png" alt="github icon" />View my repos on Github</a></li>
-          <li><a href="https://www.linkedin.com/in/keithstharp/"><img className="contact-icon" src="./images/linkedIn.png" alt="linkedIn icon" />My LinkedIn profile</a></li>
+          <li><a href="https://github.com/keiththarp"><img className="contact-icon" src="./images/github-icon.png" alt="github icon" />My repos</a></li>
+          <li><a href="https://www.linkedin.com/in/keithstharp/"><img className="contact-icon" src="./images/linkedIn.png" alt="linkedIn icon" />My LinkedIn</a></li>
         </ul>
-        <p><a href="https://bit.ly/KT-Resume">My resume.</a></p>
-
+        <br />
+        <div>
+          <p><a className="resume" href="https://bit.ly/KT-Resume">My resume.</a></p>
+        </div>
       </div>
       <div className="view-buffer"></div>
     </section>
